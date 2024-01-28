54b4227b15d28ec78f82955cc9408fbc71f1e9e4
freddyman2k <fryderyk@mantiuk.de>
2024-01-29 00:06:51 +0100

    Slight refactoring of cosine similarity reward module

--- ellm_reward.py
+++ ellm_reward.py
@@ -1,4 +1,5 @@
 import random
+from typing import List, Tuple
 
 from sentence_transformers import SentenceTransformer, util
 import torch
@@ -7,7 +8,23 @@ class ELLMRewardCalculator:
     def __init__(self, model_name='paraphrase-MiniLM-L3-v2'):
         self.model = SentenceTransformer(model_name)
 
-    def compute_cosine_similarity(self, action_description, goal_suggestions):
+    def compute_cosine_similarity(self, action_description: str, goal_suggestions: List[str]) -> Tuple[float, str]:
+        """Computes the cosine similarity between the action description and each goal suggestion. 
+        Returns the highest scoring goal suggestion and its score.
+        
+        Args:
+            action_description (str): The action description
+            goal_suggestions (list[str]): A list of goal suggestions to compare the action description to.
+            
+        Returns:
+            float: The cosine similarity score of the highest scoring goal suggestion
+            str: The highest scoring goal suggestion
+        """
+        
+        # If there are no goal suggestions, return a similarity score of 0 and no closest suggestion
+        if len(goal_suggestions) == 0:
+            return 0, None
+        
         # Embed the action description and goal suggestions
         embeddings = self.model.encode([action_description] + goal_suggestions, convert_to_tensor=True)
 
