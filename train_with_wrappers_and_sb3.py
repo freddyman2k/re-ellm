82586b65915ac5726f67389fdc0b502c3bc101c7
freddyman2k <fryderyk@mantiuk.de>
2024-02-01 16:16:52 +0100

    refactoring: delete global variables SIMILARITY_TRESHHOLD, etc, add testllm as choice for debugging

--- train_with_wrappers_and_sb3.py
+++ train_with_wrappers_and_sb3.py
@@ -10,16 +10,12 @@ from stable_baselines3.common.env_checker import check_env
 import numpy as np
 import torch
 
-from environment import CustomFrameStack, TransformObsSpace
-from llm import HuggingfacePipelineLLM, LLMGoalGenerator, TestCacheLLM, ConstantGoalGenerator, ConstantSamplerGoalGenerator
-from policy import DQNPolicy
+from llm import HuggingfacePipelineLLM, LLMGoalGenerator, ConstantGoalGenerator, ConstantSamplerGoalGenerator, TestCacheLLM
 from ellm_reward import ELLMRewardCalculator
-from utils import SaveCacheCallback, TextEmbedder
+from utils import SaveCacheCallback, TextEmbedder, make_exp_name
+from env_wrapper import CreateCompleteTextObs, RewardIfActionSimilarToGoalSuggestionsFromLastStep, EmbedTextObs, GenerateGoalSuggestions
 import text_crafter.text_crafter
 
-
-SIMILARITY_THRESHOLD = 0.99
-BATCH_SIZE = 64
 env_spec = {
     'name': 'CrafterTextEnv-v1',
     'action_space_type': 'harder',
@@ -27,14 +23,15 @@ env_spec = {
     'seed': 1,
     'dying': True,
     'length': 400, # TODO: Discuss if we should use a different length for training
-    'threshold': .99,
+    'similarity_threshold': .99,
     'check_ac_success': False,
     'novelty_bias': False,
-    'goal_generator': "LLMGoalGenerator",
-    'language_model': "mistral7binstruct",
+    'goal_generator': "ConstantGoalGenerator", #Options: "LLMGoalGenerator", "ConstantGoalGenerator", "ConstantSamplerGoalGenerator"
+    'language_model': "mistral7binstruct", #Options: "mistral7binstruct", "testllm"
     'frame_stack': 4,
 }
 
+# TODO: Do we want this to be here or in the env_wrapper?
 def make_env(name='CrafterTextEnv-v1',
                 action_space_type='harder',
                 device='cpu',  
@@ -44,106 +41,17 @@ def make_env(name='CrafterTextEnv-v1',
                 device=device)
     return env
 
+# TODO: Do we want this to be here or in the env_wrapper?
 class SharedState:
     def __init__(self):
         self.last_state_goal_suggestions = None
         self.prev_achieved_goals = []
 
-class CreateCompleteTextObs(gym.ObservationWrapper):
-    def __init__(self, env):
-        super().__init__(env)
-    """Create the complete text observation as in the paper by concatenating the text observation with the inventory and status"""
-    def observation(self, observation):
-        text_obs_complete = " ".join([observation['text_obs'], observation['inv_status']['inv'], observation['inv_status']['status']])
-        observation = {'obs': observation['obs'], 'text_obs': text_obs_complete}
-        return observation
-    
-class RewardIfActionSimilarToGoalSuggestionsFromLastStep(gym.Wrapper):
-    """Rewards the agent if the action it took is similar to one of the goal suggestions for the next state"""
-    def __init__(self, env, reward_calculator, shared_state, similarity_threshold=0.99, check_ac_success=True, novelty_bias=True, print_on_reward=False):
-        super().__init__(env)
-        self.reward_calculator = reward_calculator
-        self.similarity_threshold = similarity_threshold
-        self.shared_state = shared_state
-        self.print_on_reward = print_on_reward
-        self.check_ac_success = check_ac_success
-        self.novelty_bias = novelty_bias
-        
-        self.last_text_obs = None
-        
-    def reset(self, **kwargs):
-        obs, info = self.env.reset(**kwargs)
-        self.last_text_obs = obs['text_obs']
-        return obs, info
-        
-    def step(self, action):
-        obs, _, terminated, truncated, info = self.env.step(action)
-                
-        # Compute reward based on similarity between the actual action and the suggested ones
-        reward = 0
-        action_name = self.env.get_action_name(action)
-        intrinsic_reward, closest_suggestion = self.reward_calculator.compute_cosine_similarity(action_name, self.shared_state.last_state_goal_suggestions)
-        if intrinsic_reward > self.similarity_threshold and ((info["action_success"] and self.check_ac_success) or not self.check_ac_success):
-            reward = reward + intrinsic_reward
-            if self.novelty_bias:
-                self.shared_state.prev_achieved_goals.append(closest_suggestion)
-            
-            if self.print_on_reward:
-                # Print some info if the agent received an intrinsic reward 
-                print("\n=====================================================")
-                print("\nRewarding agent for successfully enacting a suggestion!")
-                print("\nLast text obs: ", self.last_text_obs)
-                print("\nPolicy chose the following action from the last observation: ", action_name)
-                print("\nGenerated suggestions in last step: ", self.shared_state.last_state_goal_suggestions)
-                print("\nMost similar suggestion: ", closest_suggestion)
-                print("\nCurrent text obs: ", obs['text_obs'])
-                print("\nIntrinsic reward: ", intrinsic_reward)
-                print("\nGoal suggestions after appending achieved suggestion: ", self.shared_state.prev_achieved_goals)
-                print("=====================================================\n")
-        
-        self.last_text_obs = obs['text_obs']
-        
-        return obs, reward, terminated, truncated, info
-
-class GenerateGoalSuggestions(gym.ObservationWrapper):
-    """Generate goal suggestions for the agent to pursue, by prompting a language model with the current text observation"""
-    def __init__(self, env, goal_generator, shared_state, novelty_bias=True):
-        super().__init__(env)
-        self.goal_generator = goal_generator
-        self.shared_state = shared_state
-        self.novelty_bias = novelty_bias
-        
-    def observation(self, obs):
-        goal_suggestions = self.goal_generator.generate_goals(obs['text_obs'])
-        if self.novelty_bias:
-            # Remove goals that have already been achieved in previous steps
-            goal_suggestions = [goal for goal in goal_suggestions if goal not in self.shared_state.prev_achieved_goals]
-        self.shared_state.last_state_goal_suggestions = goal_suggestions # Save for reward calculation in next step, after policy has acted
-        # TODO: Add goal suggestions to observation here, so that a goal-conditioned agent can be trained
-        return obs
-    
-class EmbedTextObs(gym.ObservationWrapper):
-    """Embed the text observation using the text embedder. Necessary for stable baselines to work with the text observation."""
-    def __init__(self, env, obs_embedder):
-        super().__init__(env)
-        self.obs_embedder = obs_embedder
-        
-        # This is a little hacky, but the original text crafter env has a observation space that does not correspond to the actual output of the env
-        self.observation_space = gym.spaces.Dict({
-            'obs': self.env.observation_space,
-            'text_obs': gym.spaces.Box(low=-np.inf, high=np.inf, shape=obs_embedder.embed("test").shape),
-        })
-
-    def observation(self, observation):
-        return {
-            'obs': observation['obs'], 
-            'text_obs': self.obs_embedder.embed(observation['text_obs'])
-            }
 
 def make_full_train_env(reward_calculator, goal_generator, obs_embedder, shared_state, device="cpu"):
     env = make_env(**env_spec, device=device)
     env = CreateCompleteTextObs(env)
-    env = RewardIfActionSimilarToGoalSuggestionsFromLastStep(env, reward_calculator, shared_state, similarity_threshold=SIMILARITY_THRESHOLD, check_ac_success=env_spec['check_ac_success'])
+    env = RewardIfActionSimilarToGoalSuggestionsFromLastStep(env, reward_calculator, shared_state, similarity_threshold=env_spec['similarity_threshold'], check_ac_success=env_spec['check_ac_success'])
     env = GenerateGoalSuggestions(env, goal_generator, shared_state, novelty_bias=env_spec['novelty_bias']) 
     env = EmbedTextObs(env, obs_embedder)
     return env
@@ -154,28 +62,13 @@ def make_full_eval_env(obs_embedder, device="cpu"):
     env = EmbedTextObs(env, obs_embedder)
     return env
 
-def make_exp_name(env_spec):
-    exp_run_str = f"run_seed-{env_spec['seed']}_{env_spec['goal_generator']}_"
-    if env_spec['goal_generator'] == "LLMGoalGenerator":
-        exp_run_str += f"{env_spec['language_model']}_"
-    if env_spec['novelty_bias']:
-        exp_run_str += "novelty-bias_"
-    else:
-        exp_run_str += "no-novelty-bias_"
-    if env_spec['check_ac_success']:
-        exp_run_str += "reward-on-ac-success_"
-    else:
-        exp_run_str += "ignore-ac-success_"
-        
-    return exp_run_str
-    
-
 def train_agent(max_env_steps=5000000, eval_every=5000, log_every=1000):
     device = "cuda" if torch.cuda.is_available() else "cpu"
     
     # Set up logging
     exp_run_str = make_exp_name(env_spec)
     eval_log_dir = f"./eval_logs/{exp_run_str}/"
+    print("Logging eval results to", eval_log_dir)
     os.makedirs(eval_log_dir, exist_ok=True)
     
     # Save env_spec to file
@@ -183,9 +76,10 @@ def train_agent(max_env_steps=5000000, eval_every=5000, log_every=1000):
         f.write(str(env_spec))
     
     if env_spec['goal_generator'] == "LLMGoalGenerator":
-        # language_model = TestCacheLLM() # for debugging other parts that do not need GPU, if you use this you don't need to submit a job to the cluster
         if env_spec['language_model'] == "mistral7binstruct":
             language_model = HuggingfacePipelineLLM("mistralai/Mistral-7B-Instruct-v0.2", cache_file="cache.pkl")
+        elif env_spec['language_model'] == "testllm":
+            language_model = TestCacheLLM() # for debugging other parts that do not need GPU, if you use this you don't need to submit a job to the cluster
         else:
             raise ValueError("Unknown language model, needs to be implemented first maybe?")
         goal_generator = LLMGoalGenerator(language_model=language_model)
