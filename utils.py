ac95ea31a671f60b911eb0505c0f9ec7322f5a19
freddyman2k <fryderyk@mantiuk.de>
2024-02-02 11:12:09 +0100

    added plotting functions to utils

--- utils.py
+++ utils.py
@@ -4,7 +4,29 @@ import matplotlib.pyplot as plt
 from stable_baselines3.common.callbacks import BaseCallback
 from llm import LLMBaseClass
 from datetime import datetime
+import numpy as np
+import pandas as pd
 
+### Utility functions for setup, training and evaluation
+
+def make_exp_name(env_spec):
+    exp_run_str = f"run_seed-{env_spec['seed']}_{env_spec['goal_generator']}_"
+    if env_spec['goal_generator'] == "LLMGoalGenerator":
+        exp_run_str += f"{env_spec['language_model']}_"
+    if env_spec['novelty_bias']:
+        exp_run_str += "novelty-bias_"
+    else:
+        exp_run_str += "no-novelty-bias_"
+    if env_spec['check_ac_success']:
+        exp_run_str += "reward-on-ac-success_"
+    else:
+        exp_run_str += "ignore-ac-success_"
+    
+    now = datetime.now()
+    # Format as YYYY-MM-DD_HH
+    date_time_str = now.strftime("%Y-%m-%d_%H")
+    exp_run_str += date_time_str  
+    return exp_run_str
 
 class TextEmbedder:
     """Uses a pretrained SBERT model to embed text into a vector representation that can be used by a SB3 Policy network. 
@@ -25,19 +47,6 @@ class TextEmbedder:
                 # TODO: Would be nice to have a cache that automatically removes the oldest or least used entries instead
                 self.embeddings[text] = embedding
             return embedding
-        
-def visualize_obs(obs, goal_suggestions=None, frame_stack=4):
-    fig, axs = plt.subplots(1, frame_stack, figsize=(5 * frame_stack, 5))
-    title = obs["text_obs"] if isinstance(obs["text_obs"], str) else f"text_obs is embedded: {obs['text_obs'].shape}" 
-    if goal_suggestions is not None:
-        goal_suggestions = '\n'.join(goal_suggestions)
-        title += f"\nGoal suggestions: {goal_suggestions}"
-    fig.suptitle(title , fontsize=16,weight="bold" )
-    for i, frame in enumerate(obs["obs"][:frame_stack]):
-        axs[i].imshow(frame)
-        axs[i].axis('off')
-        axs[i].set_title(f't={i - frame_stack + 1}', y=-0.15)
-    plt.show()
 
 class SaveCacheCallback(BaseCallback):
     def __init__(self, language_model: LLMBaseClass):
@@ -52,23 +61,49 @@ class SaveCacheCallback(BaseCallback):
         return True
     
 
-def make_exp_name(env_spec):
-    exp_run_str = f"run_seed-{env_spec['seed']}_{env_spec['goal_generator']}_"
-    if env_spec['goal_generator'] == "LLMGoalGenerator":
-        exp_run_str += f"{env_spec['language_model']}_"
-    if env_spec['novelty_bias']:
-        exp_run_str += "novelty-bias_"
-    else:
-        exp_run_str += "no-novelty-bias_"
-    if env_spec['check_ac_success']:
-        exp_run_str += "reward-on-ac-success_"
-    else:
-        exp_run_str += "ignore-ac-success_"
-    
-    now = datetime.now()
-    # Format as YYYY-MM-DD_HH
-    date_time_str = now.strftime("%Y-%m-%d_%H")
-    exp_run_str += date_time_str  
-    return exp_run_str
+
+### Plotting functions
+def moving_average(a, n=3) :
+    ret = np.cumsum(a, dtype=float)
+    ret[n:] = ret[n:] - ret[:-n]
+    return ret[n - 1:] / n
+
+def plot_rewards(rewards, checkpoint_dir, title='', time_steps=None, window_size=10):
+    x = np.arange(len(rewards)) if time_steps is None else time_steps
+    plt.plot(x, np.array(rewards), color='steelblue')
+    #plt.plot(x, np.array(rewards), 'o', color='steelblue')
+    # Calculate moving average
+    moving_avg = pd.Series(rewards).rolling(window=window_size, min_periods=1).mean().values
+    plt.plot(x, moving_avg, color = "darkred",linewidth=2.1)  
+    plt.legend(['Mean eval reward', 'Moving average'], loc='upper left')
+    plt.title(title)
+    x_label ='Episodes'  if time_steps is None else 'Time steps'
+    plt.xlabel(x_label)
+    plt.ylabel('Reward')
+    ax = plt.gca()
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    plt.gca().ticklabel_format(style='sci', axis='x', scilimits=(3,3))  # Add x 1e4 at the right end of the axis
+    plt.savefig(checkpoint_dir + "mean_episode_eval_rewards.png")
+    plt.show()
+
+def plot_moving_averages(reward_time_step_dict, title='', checkpoint_dir='eval_logs/', window_size=10):
+    for model_name, (rewards, time_steps) in reward_time_step_dict.items():
+        # Calculate moving average
+        moving_avg = pd.Series(rewards).rolling(window=window_size, min_periods=1).mean().values
+        plt.plot(time_steps, moving_avg, linewidth=2.1, label=f'{model_name}')  
+
+    plt.legend(loc='upper left')
+    plt.title(title)
+    plt.xlabel('Time steps')
+    plt.ylabel('Reward')
+    ax = plt.gca()
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    plt.gca().ticklabel_format(style='sci', axis='x', scilimits=(3,3))  # Add x 1e4 at the right end of the axis
+    plt.savefig(checkpoint_dir + "moving_avg_model_comparision.png")
+    plt.show()
+
+
 
 
